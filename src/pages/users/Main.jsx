cd8e2c2cab75d3b8dd685b2b2a3b1f0b7c5d99d1
thanawat4115 <thanawat.na@kkumail.com>
2022-01-14 17:25:09 +0700

    UI หน้าแรก
    -สร้าง UI หน้าแรก ที่ไฟล์ Main.jsx เสร็จ
    -กำหนดขนาดของ responsive ในไฟล์ Main.css

--- src/pages/users/Main.jsx
+++ src/pages/users/Main.jsx
@@ -3,11 +3,13 @@ import './Main.css'
 import Main_image from '../../assets/main_image.png'
 import Logo from '../../components/Logo'
 import Button from '../../components/Button'
+import {Link} from 'react-router-dom'
 
 const Main = () => {
     return (
         <div className='main_container'>
-            <Logo/>
+            <Logo />
+            <Link to="/login" className='main_login'>ลงชื่อเข้าใช้</Link>
             <img className='main_img' src={Main_image}/>
         </div>
     )
