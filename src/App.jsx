c2a5f56ae428cf4dc1817c08d20261a3a36f93c4
thanawat4115 <thanawat.na@kkumail.com>
2022-01-14 18:49:14 +0700

    LoginPage(ยังไม่เสร็จ)
    -เพจ Login คร่าวๆ

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Main from './pages/users/Main'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Detail from './pages/users/Detail'
+import Login from './pages/users/Login'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <div className="app_container">
           <Routes>
               <Route path='/' element={<Main/>}/>
+              <Route path='/login' element={<Login/>}/>
               <Route path='/detail' element={<Detail/>}/>
           </Routes>
       </div>
--- src/pages/users/Main.jsx
+++ src/pages/users/Main.jsx
@@ -4,11 +4,13 @@ import Main_image from '../../assets/main_image.png'
 import Logo from '../../components/Logo'
 import Button from '../../components/Button'
 import {Link} from 'react-router-dom'
-
+import { motion } from 'framer-motion'
 const Main = () => {
     return (
         <div className='main_container'>
-            <Logo />
+            <motion.div layoutId='test'>
+                <Logo />
+            </motion.div>
             <Link to="/login" className='main_login'>ลงชื่อเข้าใช้</Link>
             <img className='main_img' src={Main_image}/>
         </div>
