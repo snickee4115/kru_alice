2fcd59a06c00e82b8e3920028abf4c1d992ca902
thanawat4115 <thanawat.na@kkumail.com>
2022-01-15 19:13:54 +0700

    UI หน้าล็อคอิน
    -ทำ UI หน้าล็อคอิน
    -ลิงค์หน้าแรกที่ logo
    -responsive
    -แก้ width: 800px; ที่ app_container

--- src/pages/users/Login.jsx
+++ src/pages/users/Login.jsx
@@ -3,14 +3,28 @@ import './Login.css'
 import Img_nerd from '../../assets/nerd_image.png'
 import Logo from '../../components/Logo'
 import {motion} from 'framer-motion'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
         <div className='login_container'>
-            <motion.div transition={{duration:1}} layoutId='test' style={{rotate:-20}} className="login_logo">
-                <Logo/>
-            </motion.div>
-            <img src={Img_nerd} />
+            <Link to='/'>
+                <div className='login_logo_container'>
+                        <motion.div
+                            transition={{ duration: 1 }}
+                            layoutId='test'
+                            style={{ rotate: -20,  }}
+                            className="login_logo">
+                            <Logo/>
+                        </motion.div>
+                        <img className='login_img' src={Img_nerd} />
+                </div>
+            </Link>
+            <form className="login_form_container">
+                <div>เบอร์โทรศัพท์</div>
+                <input type="number" />
+                <button>ลงชื่อเข้าใช้</button>
+            </form>
         </div>
     )
 }
