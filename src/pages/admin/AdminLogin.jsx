2d702457a80f9a74a99854da179c35d242f8885c
thanawat4115 <thanawat.na@kkumail.com>
2022-02-15 16:35:28 +0700

    เปลี่ยน admin logo เป็นรูปแทน component และแก้ Responsive นิดหน่อย
    - เปลี่ยน admin logo เป็นรูปแทน component เพราะมีปัญหาเวลาปรับขนาดแล้ว logo จะเพี้ยนทำให้ต้องแก้ให้เหมาะใหม่
    -แก้ responsive ตรงระยะห่างโลโก้กับฟอร์ม

--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './AdminLogin.css'
 import Button from '../../components/Button'
-import AdminLogo from '../../components/AdminLogo'
+import AdminLogo from '../../assets/admin_logo.png'
 
 const AdminLogin = () => {
   return (
     <div className='alogin_container'>
         <div  className='alogo'>
-          <AdminLogo/>
+          <img src={AdminLogo} />
         </div>
       <form className="alogin_form_container">
         <div>username</div>
