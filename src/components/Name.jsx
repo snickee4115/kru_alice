06bbbb763c6f9d9b7ddc3b1a8f43b2bb194b3d20
Tineoo <76898743+Tineoo@users.noreply.github.com>
2022-03-01 16:28:25 +0700

    create componant Name and do page datalist
    สร้างคอมโพแน้น name แล้วก็ทำหน้า datalist ต่อ

---
+++ src/components/Name.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Name.css'
+
+function Name({name}) {
+  return (
+    <div>
+        <div className='namestu'>{name}</div>
+    </div>
+  )
+}
+
+export default Name
--- src/pages/users/Datalist.jsx
+++ src/pages/users/Datalist.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Datalist.css'
 import Undo from '../../assets/undo.png'
 import { Link } from 'react-router-dom'
-
+import Name from '../../components/Name.jsx'
 
 
 const Datalist = () => {
@@ -18,11 +18,14 @@ const Datalist = () => {
         {date:"SAT 12/06/2021 ",tim:"1 H. 0 S."},
         {date:"SAT 12/06/2021 ",tim:"1 H. 0 S."}
     ]
-
+    const re = [{result:"5 Hr. 0 Sec",remain:"5 Hr. 0 Sec."}]
+    
     return (
         
         <div className='datalist_main'>
-            <div className='datalist_name'>น้องเจแปน</div>
+            <div className='datalist_name' >
+                <Name name='น้องเจแปน'/>
+            </div>
             
             
             <Link  to ='/detail'>
@@ -40,11 +43,26 @@ const Datalist = () => {
 
             
             <div className='datalist_data'>
-                <div>{ti.map((value,index) => <div>ครั้งที่{index+1} {value.date} {value.tim}</div>  )}</div>
+                    {/* <div className='list_data'> 
+                        <div className='datalist_count'>{ti.map((value,index) => <div>ครั้งที่{index+1} </div>  )}</div>
+                        <div className='datalist_date'>{ti.map((value,index) => <div>{value.date} </div>)}</div>
+                        <div className='datalist_time'>{ti.map((value,index) => <div>{value.tim} </div>)}</div>
+                    </div> */}
+                    {ti.map((value,index ) => <div className='list_data'> 
+                        <div className='datalist_count'>ครั้งที่{index+1}</div>  
+                        <div className='datalist_date'>{value.date}</div>    
+                        <div className='datalist_time'>{value.tim} </div>    </div>)}
+                    <div className='datalist_result'> 
+                        {re.map((value,index) => <div>รวม{value.result}</div>)}
+                        {re.map((value,index) => <div>เหลือ{value.result}</div>)}
+                    </div>
 
-               {/* <div className='datalist_count'>ครั้งที่ 1 </div>
-               <div className='datalist_count'>ครั้งที่ 2 </div>  */}
-              
+                    <div className='datalist_infomation'>
+                        ddd
+                    </div>
+
+
+
             </div>
 
 
