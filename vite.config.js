4f3b76eaa43e097af33a9da3282c5c37f9d73191
thanawat4115 <thanawat.na@kkumail.com>
2022-04-15 15:32:32 +0700

    pwa_test

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      mode: "development",
+      base: "/",
+      includeAssets: ["/favicon.png"],
+      manifest: {
+        name: "Kru Alice",
+        short_name: "Kru Alice",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-384x384.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    })
+  ],
   server: {
     host: true,
 },
